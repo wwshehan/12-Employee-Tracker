e92d24316d997818fcd7d2fa945f09e39816ccdf
Warren Shehan <wwshehan@gmail.com>
2020-03-08 11:01:53 -0400

    updated js

---
+++ index.js
@@ -0,0 +1,65 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "CMS_db"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    start()
+    });
+
+const start=() => {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+        "View all Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
+        ]
+    })
+    .then((answer) => {
+        switch (answer.action) {
+            case "View all Employees":
+            employeeViewAll();
+            break;
+            case "View All Employees By Department":
+            employeeByDept();
+            break;
+            case "View All Employees By Manager":
+            employeeByManager();
+            break;
+            case "Add Employee":
+            addEmployee();
+            break;
+            case "Add Department":
+            addDepartment ();
+            break;
+            case "Add Role":
+            addRole ();
+            break;
+            case "Update Employee Role":
+            updateRole();
+            break;
+        }
+    })
+}
+    
---
+++ tracker.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS CMS_db;
+
+CREATE DATABASE CMS_db;
+USE CMS_db;
+
+CREATE TABLE departments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (6,3) NOT NULL,
+    department_id INT
+);
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
